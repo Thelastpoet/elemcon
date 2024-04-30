ea3df585aff7e321a63d4bc4ac6ee2cf23f5acfb
Emmanuel Chekumbe <emcie4@gmail.com>
2024-04-30 09:52:12 +0300

    temporarily disabled script

--- jumbo-navbar.php
+++ jumbo-navbar.php
@@ -40,7 +40,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_widget_styles', 20 );
 
 
 function enqueue_widget_scripts() {
-    wp_enqueue_script('navbar-widget-script', plugins_url('assets/js/navbar-widget.js', __FILE__), ['elementor-editor'], '1.0.0', true);
+   // wp_enqueue_script('navbar-widget-script', plugins_url('assets/js/navbar-widget.js', __FILE__), ['elementor-editor'], '1.0.0', true);
     
 }
 add_action('elementor/editor/before_enqueue_scripts', 'enqueue_widget_scripts');
